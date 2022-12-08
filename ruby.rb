3a5002aa86ef048ec65cd93970e02cc4b189a1cf
drewrolla <andrewong007@gmail.com>
2022-12-08 15:52:02 -0500

    first commit

---
+++ ruby.rb
@@ -0,0 +1,10 @@
+print "What's your first name? "
+first_name = gets.chomp
+print "What is your last name? "
+last_name = gets.chomp
+print "What city are you from? "
+city = gets.chomp
+print "What state? "
+state = gets.chomp
+
+puts "Your name is #{first_name} #{last_name} and you're from #{city}, #{state}!"
