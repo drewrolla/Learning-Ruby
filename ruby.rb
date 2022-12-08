cf0cd6c074c6a3c40f9b02e206356581175bec31
drewrolla <andrewong007@gmail.com>
2022-12-08 16:01:53 -0500

    added capitalization to code

--- ruby.rb
+++ ruby.rb
@@ -1,11 +1,11 @@
 print "What's your first name? "
-first_name = gets.chomp
+first_name = gets.chomp.capitalize!
 print "What is your last name? "
-last_name = gets.chomp
+last_name = gets.chomp.capitalize!
 print "What city are you from? "
-city = gets.chomp
+city = gets.chomp.capitalize!
 print "What state? "
-state = gets.chomp
+state = gets.chomp.upcase!
 
 puts "Your name is #{first_name} #{last_name} and you're from #{city}, #{state}!"
 
